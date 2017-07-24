b5f0463550ad174c3c7e90df6f5d6d1e6ed73e35
Juna Khatri <junakhatri@gmail.com>
2017-07-24 15:12:35 -0400

    created loginViewModel and RegisterViewModel

--- Capstone.Web/Models/LoginViewModel.cs
+++ Capstone.Web/Models/LoginViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,5 +8,12 @@ namespace Capstone.Web.Models
 {
     public class LoginViewModel
     {
+        [Required(ErrorMessage = "This field is required.")]
+        [Display(Name = "User Name:")]
+        public string UserName { get; set; }
+
+        [Required(ErrorMessage = "This field is required.")]
+        [Display(Name ="Password:")]
+        public string Password { get; set; }
     }
 }
---
+++ Capstone.Web/Models/RegisterViewModel.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace Capstone.Web.Models
+{
+    public class RegisterViewModel
+    {
+        [Required(ErrorMessage = "This field is required.")]
+        [Display(Name = "First Name:")]
+        public string FirstName { get; set; }
+
+        [Required(ErrorMessage = "This field is required.")]
+        [Display(Name = "Last Name:")]
+        public string LastName { get; set; }
+
+        [Required(ErrorMessage = "This field is required.")]
+        [DataType(DataType.EmailAddress)]
+        [Display(Name = "Email")]
+        [EmailAddress(ErrorMessage = "Invalid Email")]
+        public string EmailAddress { get; set; }
+
+
+        [Required(ErrorMessage = "This field is required")]
+        [MinLength(8, ErrorMessage = "Password must be at least 8 characters")]
+        [RegularExpression(@"^(?=(.*\d){2})(?=.*[a-z])(?=.*[A-Z])(?=.*[^a-zA-Z\d]).{8,}$", ErrorMessage = "Password is too weak")]
+        [Display(Name = "Password:")]
+        public string Password { get; set; }
+
+        [Required(ErrorMessage = "This field is required")]
+        [System.ComponentModel.DataAnnotations.Compare("Password", ErrorMessage = "Passwords do not match")]
+        [Display(Name = "Confirm Password:")]
+        public string ConfirmPassword { get; set; }
+    }
+}
