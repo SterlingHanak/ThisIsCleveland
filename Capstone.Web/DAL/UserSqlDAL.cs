a6b2ab72b924bd2b4836ae0acc8c73a322b798d9
Launa Greer <greerlauna@gmail.com>
2017-07-25 21:39:20 -0400

    Modified Register view and completed 'GetUser(string username)' method in UserSqlDAL while adding private method PopulateUserObject(SqlDataReader reader)

--- Capstone.Web/DAL/UserSqlDAL.cs
+++ Capstone.Web/DAL/UserSqlDAL.cs
@@ -3,11 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using Capstone.Web.Models;
+using System.Data.SqlClient;
 
 namespace Capstone.Web.DAL
 {
     public class UserSqlDAL : IUserDAL
     {
+        readonly string SQL_GetUser = "SELECT * FROM user WHERE username = @username;";
+        private string connectionString;
+
+        public UserSqlDAL(string connectionString)
+        {
+            this.connectionString = connectionString;
+        }
+
         public bool ChangePassword(string username, string hashedPassword)
         {
             throw new NotImplementedException();
@@ -20,12 +29,44 @@ namespace Capstone.Web.DAL
 
         public User GetUser(string userName)
         {
-            throw new NotImplementedException();
+            try
+            {
+                using(SqlConnection conn = new SqlConnection(connectionString))
+                {
+                    conn.Open();
+                    SqlCommand cmd = new SqlCommand(SQL_GetUser, conn);
+                    cmd.Parameters.AddWithValue("username", userName);
+
+                    User thisUser = new User();
+                    SqlDataReader reader = cmd.ExecuteReader();
+                    while(reader.Read())
+                    {
+                        thisUser = PopulateUserObject(reader);
+                    }
+                    return thisUser;
+                }
+            }
+            catch(SqlException)
+            {
+                throw;
+            }
         }
 
         public bool RegisterUser(User newUser)
         {
             throw new NotImplementedException();
+        }
+
+        private User PopulateUserObject(SqlDataReader reader)
+        {
+            User user = new User();
+            user.FirstName = Convert.ToString(reader["first_name"]);
+            user.LastName = Convert.ToString(reader["last_name"]);
+            user.Username = Convert.ToString(reader["username"]);
+            user.Password = Convert.ToString(reader["password"]);
+            user.Salt = Convert.ToString(reader["salt"]);
+
+            return user;
         }
     }
 }
