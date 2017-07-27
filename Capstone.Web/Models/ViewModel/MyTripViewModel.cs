11e4b79bd972b47c9490459d0734a74520dcddae
Juna Khatri <junakhatri@gmail.com>
2017-07-27 12:00:43 -0400

    added mytripviewmodel, registerviewmodel

---
+++ Capstone.Web/Models/ViewModel/MyTripViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Capstone.Web.Models
+{
+    public class MyTripViewModel
+    {
+        public List<Landmark> Landmark { get; set; }
+        public List<string> Categories { get; set; }
+        public Trip NewTrip { get; set; }
+    }
+}
