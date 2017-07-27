78286b16a4a9d4682b0505880b03408cb75df379
Sterling Hanak <hanak@roadrunner.com>
2017-07-27 18:40:05 -0400

    update

--- database/data.sql
+++ database/data.sql
@@ -6,7 +6,7 @@ BEGIN TRAN;
 
 INSERT INTO landmark VALUES ('TownHall', '1909 W 25th St, Cleveland, OH 44113', '216.344.9400', 'Locally sourced & innovative bar bites & vegan options served in a contemporary setting with patio.', null, 4.3, 2, null, 'townhallohiocity.com');
 INSERT INTO landmark VALUES ('Barrio', '503 Prospect Ave E, Cleveland, OH 44115', '216.862.4652', 'Local chain serving signature & build-your-own tacos, plus cocktails, in a hip, casual space.', null, 4.4, 1, null, 'barrio-tacos.com');
-INSERT INTO landmark VALUES ('Tilted Kilt Pub and Eatery', '21 Prospect Ave E, Cleveland, OH 44115', '216.771.5458', 'UK-inspired pub chain with kilt-clad waitresses serving fish 'n' chips & bar food.', null, 4.2, 2, null, 'tiltedkilt.com');
+INSERT INTO landmark VALUES ('Tilted Kilt Pub and Eatery', '21 Prospect Ave E, Cleveland, OH 44115', '216.771.5458', 'UK-inspired pub chain with kilt-clad waitresses serving fish ''n'' chips & bar food.', null, 4.2, 2, null, 'tiltedkilt.com');
 INSERT INTO landmark VALUES ('Shooters On the Water', '1148 Main Ave, Cleveland, OH 44113', '216.861.6900', 'A high-volume venue offering a long menu of familiar fare & a patio showing a sound & light show.', null, 4.5, 2, null, 'shootersflats.com');
 INSERT INTO landmark VALUES ('Hofbräuhaus', '1550 Chester Ave, Cleveland, OH 44115', '216.621.2337', 'German microbrewery with an on-site restaurant & beer garden offering traditional eats & live music.', null, 4.5, 3, null, 'hofbrauhauscleveland.com');
 INSERT INTO landmark VALUES ('TOMO Sushi & Hibachi Restaurant', '1293 W 9th St, Cleveland, OH 44113', '216.696.4444', 'Huge 4-floor venue offering Japanese hibachi fare & sushi plus lounges with DJs, music & dancing.', null, 3.5, 3, null, 'tomohibachiandsushi.com');
