0b30d35a66d60fbcb005f88f1313fd50a8c1ffc6
Launa Greer <greerlauna@gmail.com>
2017-07-28 11:48:29 -0400

    Added ThingsToDoController and associated view. Added temporary background colors to divs in NewTrip view.

--- Capstone.Web/Controllers/PlanTripController.cs
+++ Capstone.Web/Controllers/PlanTripController.cs
@@ -25,14 +25,35 @@ namespace Capstone.Web.Controllers
             {
                 RedirectToAction("Login", "Users");
             }
-            MyTripViewModel myTripViewModel = new MyTripViewModel();
-    
-            return View("NewTrip");
+            MyTripViewModel myTripViewModel = PopulateMyTripViewModel();
+            return View("NewTrip", myTripViewModel);
         }
 
         public ActionResult LandmarkDetails(int landmarkId)
         {
             return PartialView("_LandmarkDetails");
+        }
+
+        private MyTripViewModel PopulateMyTripViewModel()
+        {
+            MyTripViewModel myTripViewModel = new MyTripViewModel();
+
+            // Retrieve all categories
+            myTripViewModel.Categories = landmarkDAL.GetAllCategories();
+
+            // Retrieve all landmarks and their properties
+            List<Landmark> allLandmarks = landmarkDAL.GetAllLandmarks();
+            foreach (Landmark landmark in allLandmarks)
+            {
+                landmark.Schedule = landmarkDAL.GetSchedule(landmark.Id);
+                landmark.Highlights = landmarkDAL.GetLandmarkHighlights(landmark.Id);
+                landmark.Categories = landmarkDAL.GetLandmarkCategories(landmark.Id);
+            }
+
+            // Initialize new trip
+            myTripViewModel.NewTrip = new Trip();
+
+            return myTripViewModel;
         }
     }
 }
---
+++ Capstone.Web/Controllers/ThingsToDoController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Capstone.Web.Models;
+using Capstone.Web.DAL;
+
+namespace Capstone.Web.Controllers
+{
+    public class ThingsToDoController : CityToursController
+    {
+        private IUserDAL userDAL;
+
+        public ThingsToDoController(IUserDAL userDAL) : base(userDAL)
+        {
+            this.userDAL = userDAL;
+        }
+
+        public ActionResult Category(int id)
+        {
+            return View("Category", id);
+        }
+    }
+}
