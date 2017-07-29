52c9d475dc3ca190ceb0d3616db88ec2093cbea5
Sterling Hanak <hanak@roadrunner.com>
2017-07-29 13:32:29 -0400

    started javascript for category view

--- Capstone.Web/Scripts/CategoryPageFilter.js
+++ Capstone.Web/Scripts/CategoryPageFilter.js
@@ -1,1 +1,8 @@
-﻿
+﻿$(document).ready(function () {
+
+    function avgRating() {
+
+        $("#checkbox-rating-1").on("click", function () {
+            if ($(this).is(':checked'))
+        }
+    }
