5f8b08a29e7709135949d82f45545f6bcf3cdf56
Launa Greer <greerlauna@gmail.com>
2017-07-30 10:08:01 -0400

    Added 'GetAllUserTrips' and 'SaveNewTrip' methods to ITripDAL

--- Capstone.Web/DAL/ITripDAL.cs
+++ Capstone.Web/DAL/ITripDAL.cs
@@ -3,10 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Capstone.Web.Models;
 
 namespace Capstone.Web.DAL
 {
-    interface ITripDAL
+    public interface ITripDAL
     {
+        List<Trip> GetAllUserTrips(string username);
+        bool SaveNewTrip(Trip trip, string username);
     }
 }
