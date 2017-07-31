89e43e7a73c624ef027e525baa18bfc081440161
Sterling Hanak <hanak@roadrunner.com>
2017-07-31 17:54:39 -0400

    Added filters to a collapsable side-bar in the category page

--- Capstone.Web/Scripts/CategoryPageFilter.js
+++ Capstone.Web/Scripts/CategoryPageFilter.js
@@ -29,7 +29,21 @@
 
     });
 
+        $("#openCatBtn").on("click", function (event) {
+            document.getElementById("mySidenav").style.width = "250px";
+        });
 
+        $("#closeCatBtn").on("click", function (event) {
+            document.getElementById("mySidenav").style.width = "0px";
+        });
+
+        function openNav() {
+            document.getElementById("mySidenav").style.width = "250px";
+        }
+
+        function closeNav() {
+            document.getElementById("mySidenav").style.width = "0";
+        }
 
 
 
